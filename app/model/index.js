07e453243cb84c4d0c62f38a785f40a468c637a9
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2023-10-09 20:43:12 +0700

    reset project from node js env to pure html css js

--- app/model/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('sequelize')
-const mysql = require('mysql2')
-
-const connection = new sequelize.Sequelize('capstone', 'root', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = connection
